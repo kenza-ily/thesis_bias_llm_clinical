e3c1bc0cefa7a209b52d828fe8737652109c36c7
kenza-ily <kenzabenkirane12@gmail.com>
2024-08-20 18:40:56 +0100

    gpt4 done, updated .sh and haiku started

--- run_fw2.sh
+++ run_fw2.sh
@@ -10,17 +10,23 @@
 
 echo "START"
 module load python3/recommended
-echo "loaded python"
+echo "Loaded Python"
+
 # Activate the environment
-source /home/ucabkbe/bias_llm_clinical_challenge/new_venv/bin/activate
+source /home/ucabkbe/bias_llm_clinical_nle/new_env_2/bin/activate
 echo "Environment activated"
-cd /home/ucabkbe/bias_llm_clinical_challenge/
-# Start the server
-echo "OLLAMA"
-echo "Ollama serving"
+
+cd /home/ucabkbe/bias_llm_clinical_nle/
+
+# Start the server in the background
+echo "Starting Ollama server..."
 ollama serve > out 2>&1 &
-# pull models directly here
-echo "Ollama pulling"
+
+# Wait for the server to start
+sleep 10  # Adjust the sleep time if needed
+
+# Pull models directly here
+echo "Pulling models with Ollama..."
 ollama pull mistral:nemo
 ollama pull mixtral:8x22b
 ollama pull mistral:7b
@@ -29,8 +35,9 @@ ollama pull llama3:70b
 ollama pull llama3.1
 ollama pull gemma2:2b
 ollama pull gemma2
+
 echo "Running the script"
 # ! HYPERPARAMETERS
-python scripts/run_exp2.py GxE 2 "test0_cluster" "open"
+python scripts/run_exp2.py GxE 2 "test3_cluster" "open"
 echo "Script running done"
 echo "END"
