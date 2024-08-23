f7a665736eac0a85199013dab465edd425398f25
kenza-ily <kenzabenkirane12@gmail.com>
2024-08-23 17:32:40 +0100

    run.sh old ciao

--- run_fw2.sh
+++ run_fw2.sh
@@ -11,30 +11,31 @@
 echo "START"
 module load python3/recommended
 echo "Loaded Python"
+# export PATH="$PATH:/home/ucabkbe/.ollama"
+
+cd /home/ucabkbe/bias_llm_clinical_nle/
 
 # Activate the environment
 source /home/ucabkbe/bias_llm_clinical_nle/new_env_2/bin/activate
 echo "Environment activated"
-
-cd /home/ucabkbe/bias_llm_clinical_nle/
-
+/home/ucabkbe/.ollama/ollama serve
 # Start the server in the background
 echo "Starting Ollama server..."
-ollama serve > out 2>&1 &
+OLLAMA_HOST=127.0.0.1:12212 /home/ucabkbe/.ollama/ollama serve
 
 # Wait for the server to start
 sleep 10  # Adjust the sleep time if needed
 
 # Pull models directly here
 echo "Pulling models with Ollama..."
-ollama pull mistral:nemo
-ollama pull mixtral:8x22b
-ollama pull mistral:7b
-ollama pull llama3:8b
-ollama pull llama3:70b
-ollama pull llama3.1
-ollama pull gemma2:2b
-ollama pull gemma2
+/home/ucabkbe/.ollama/ollama pull mistral:nemo
+/home/ucabkbe/.ollama/ollama pull mixtral:8x22b
+/home/ucabkbe/.ollama/ollama pull mistral:7b
+/home/ucabkbe/.ollama/ollama pull llama3:8b
+/home/ucabkbe/.ollama/ollama pull llama3:70b
+/home/ucabkbe/.ollama/ollama pull llama3.1
+/home/ucabkbe/.ollama/ollama pull gemma2:2b
+/home/ucabkbe/.ollama/ollama pull gemma2
 
 echo "Running the script"
 # ! HYPERPARAMETERS
