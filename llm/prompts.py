9fd0958e5df147381e2e237782d8d4cc448b41e3
kenza-benkirane <kenzabenkirane12@gmail.com>
2024-09-03 10:54:49 +0100

    exp5

--- llm/prompts.py
+++ llm/prompts.py
@@ -5,15 +5,19 @@ def load_prompt(experiment, filename):
     with open(prompt_path, 'r') as file:
         return file.read().strip()
 
+
+# ============ PIPELINE 0
 # Experiment 0
 exp0_system_prompt = load_prompt('experiment0', 'exp0_system_prompt.txt')
 exp0_user_prompt = load_prompt('experiment0', 'exp0_user_prompt.txt')
 
+# ============ PIPELINE 1
 # Experiment 1
 exp1_system_prompt = load_prompt('experiment1', 'exp1_system_prompt.txt')
 exp1_user_prompt = load_prompt('experiment1', 'exp1_user_prompt.txt')
 exp1_specific_question = load_prompt('experiment1', 'exp1_specific_question.txt')
 
+# ============ PIPELINE 2
 # Experiment 2
 exp2_system_prompt = load_prompt('experiment2', 'exp2_system_prompt.txt')
 exp2_user_prompt = load_prompt('experiment2', 'exp2_user_prompt.txt')
@@ -25,3 +29,8 @@ exp3_user_prompt = load_prompt('experiment3', 'exp3_user_prompt.txt')
 # # Experiment 4
 exp4_system_prompt = load_prompt('experiment4', 'exp4_system_prompt.txt')
 exp4_user_prompt = load_prompt('experiment4', 'exp4_user_prompt.txt')
+
+# ============ PIPELINE 3
+# Experiment 5
+exp5_system_prompt = load_prompt('experiment5', 'exp5_system_prompt.txt')
+exp5_user_prompt = load_prompt('experiment5', 'exp5_user_prompt.txt')
