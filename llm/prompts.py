15b4c4376e4c374f780e8d21f485fbba1cce46d7
kenza-benkirane <kenzabenkirane12@gmail.com>
2024-09-21 10:04:56 +0100

    prompts ft update again

--- llm/prompts.py
+++ llm/prompts.py
@@ -30,7 +30,18 @@ exp3_user_prompt = load_prompt('experiment3', 'exp3_user_prompt.txt')
 exp4_system_prompt = load_prompt('experiment4', 'exp4_system_prompt.txt')
 exp4_user_prompt = load_prompt('experiment4', 'exp4_user_prompt.txt')
 
-# ============ PIPELINE 3
+# ============ PIPELINE 3 -> NO LABEL
 # Experiment 5
 exp5_system_prompt = load_prompt('experiment5', 'exp5_system_prompt.txt')
 exp5_user_prompt = load_prompt('experiment5', 'exp5_user_prompt.txt')
+
+
+# ======== EXPERIMENT 6 -> FT
+
+## MCQ
+exp6_system_prompt_mcq= load_prompt('experiment6_ft_mcq', 'exp6_system_prompt_mcq.txt')
+exp6_user_prompt_mcq = load_prompt('experiment6_ft_mcq', 'exp6_user_prompt_mcq.txt')
+
+## XPL
+exp6_system_prompt_xpl = load_prompt('experiment6_ft_xpl', 'exp6_system_prompt_xpl.txt')
+exp6_user_prompt_xpl = load_prompt('experiment6_ft_xpl', 'exp6_user_prompt_xpl.txt')
